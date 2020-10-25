5e365352216bb7ff22c38ce1684ee532679dcf22
Anastasiia Bandurova <banduova.nastya@gmail.com>
2020-10-24 20:53:24 -0700

    added file to .gitignore and redo H_W_6_1

--- src/HW_6_1.java
+++ src/HW_6_1.java
@@ -4,10 +4,8 @@ public class HW_6_1 {
         int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
 
         int sum = 0;
-        int i = 0;
-        while (i < array.length) {
+        for (int i = 0; i < array.length; i++){
             sum += array[i];
-            i++;
         }
         System.out.println(sum);
     }
